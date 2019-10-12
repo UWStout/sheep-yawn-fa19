fe1f498626163548f86fe4630cd6bda9ccff5426
Smith <smitha8687@my.uwstout.edu>
2019-10-12 16:06:23 -0500

    Collider box added to Woolf enemy. Box needs resizing.

--- src/scenes/TestSheepMove.js
+++ src/scenes/TestSheepMove.js
@@ -11,36 +11,43 @@ import Tree from '..//sprites/Tree'
 class TestSheepMove extends Phaser.Scene {
   init (data) { }
 
+  // Grabs images and other material needed for the scene before any
+  // Functions are runned
   preload () {
     this.load.image('sheepImage', 'assets/images/woolhemina_testSprite_128.png')
     this.load.image('treeImage', 'assets/images/testTreeAsset1.png')
     this.load.image('woolfImage', 'assets/Test Art/testAsset_wolfEnemy (3).png')
   }
 
+  // Creates objects and other items used within the scene
+  // Not immeditally added to scene
   create () {
+    // Creation of sheep character (Main Character)
     this.player = new Woolhemina({
       scene: this,
       x: 400,
       y: 350
     })
+    // Creation of obstical, tree
     this.testTree = new Tree({
       scene: this,
       x: 200,
       y: 300
     })
+    // Creation of enemy, Woolf
     this.testWoolf = new Woolf({
       scene: this,
-      x: 500,
+      x: 600,
       y: 300
     })
 
-    // add Woolhemina and set physics
+    // add Woolhemina to scene and set physics
     this.add.existing(this.player)
     this.physics.add.existing(this.player)
     this.player.body.collideWorldBounds = true
     this.player.body.setSize(50, 105, 20, 20)
 
-    // add test tree and set physics
+    // add test tree to scene and set physics
     this.add.existing(this.testTree)
     this.physics.add.existing(this.testTree)
     this.testTree.body.setSize(256, 256, 0, 0)
@@ -48,11 +55,17 @@ class TestSheepMove extends Phaser.Scene {
     this.testTree.body.setImmovable(true)
     this.testTree.body.allowGravity = false
 
-    // Adds woolf enemy to scene
+    // Adds woolf enemy to scene and set up physics
     this.add.existing(this.testWoolf)
+    this.physics.add.existing(this.testWoolf)
+    this.testWoolf.body.setSize(250, 215, 0, 0)
+    // this.testWoolf = this.physics.add.staticGroup()
+    this.testWoolf.body.setImmovable(true)
+    this.testWoolf.body.allowGravity = false
 
     this.physics.add.collider(this.player, this.testTree)
-    // this.game.physics.arcade.collide(this.player, this.testTree)
+    this.physics.add.collider(this.player, this.testWoolf)
+    // this.game.physics.arcade.collide(this.player, this.testTree, this.testWoolf)
 
     // Setup the key objects
     this.setupKeyboard()
