aa1101715e8bbde490065a820959c7f6d2949ac3
Aumann-Weyenberg <aumann-weyenbek2486@my.uwstout.edu>
2019-10-16 12:30:02 -0500

    Merged code and can walk behind and collide tree

--- src/scenes/TestSheepMove.js
+++ src/scenes/TestSheepMove.js
@@ -11,8 +11,7 @@ import Tree from '..//sprites/Tree'
 class TestSheepMove extends Phaser.Scene {
   init (data) { }
 
-  // Grabs images and other material needed for
-  // The scene before any functions are runned
+  // Grabs images and other material needed for the scene before any functions are ran
   preload () {
     this.load.image('sheepImage', 'assets/images/woolhemina_testSprite_128.png')
     this.load.image('treeImage', 'assets/images/testTreeAsset1.png')
@@ -23,19 +22,16 @@ class TestSheepMove extends Phaser.Scene {
     this._default_boar_health = 60
     this._default_bat_health = 30
 
-    // Default yawn circumfrance increase size
+    // Default yawn circumferance increase size
     this._yawn_scale = 1.0
 
     // Sets amount that the yawn circle can increase
-    // To
     this._yawn_size_check = 1.5
   }
 
   // Creates objects and other items used within the scene
-  // Not immeditally added to scene
+  // Not immediately added to scene
   create () {
-<<<<<<< HEAD
-    /** 
     var graphics = this.add.graphics({ fillStyle: { color: 0x2266aa } })
     const point3 = new Phaser.Geom.Point(400, 428)// bottom
     graphics.fillPointShape(point3, 3)
@@ -43,33 +39,34 @@ class TestSheepMove extends Phaser.Scene {
     graphics.fillPointShape(point2, 3)
     const point1 = new Phaser.Geom.Point(500, 419)// foot
     graphics.fillPointShape(point1, 3)
-    */
-=======
     // Creation of sheep character (Main Character)
->>>>>>> 5a9f160b4b403915e73781d4da8e52fa6ca5762e
     this.player = new Woolhemina({
       scene: this,
       x: 400,
       y: 350
     })
-    // Creation of obstical, tree
+    // Creation of test tree
     this.testTree = new Tree({
       scene: this,
       x: 200,
       y: 300
     })
-<<<<<<< HEAD
-    
-    let movingVertical = false
-=======
     // Creation of enemy, Woolf
     this.testWoolf = new Woolf({
       scene: this,
       x: 600,
       y: 300
     })
->>>>>>> 5a9f160b4b403915e73781d4da8e52fa6ca5762e
+    // Adds woolf enemy to scene and set up physics
+    this.add.existing(this.testWoolf)
+    this.physics.add.existing(this.testWoolf)
+    this.testWoolf.body.setSize(250, 180, true)
+    // this.testWoolf = this.physics.add.staticGroup()
+    this.testWoolf.body.setImmovable(true)
+    this.testWoolf.body.allowGravity = false
 
+    // To check if Woolhemina is moving up and down
+    let movingVertical = false
     // add Woolhemina to scene and set physics
     this.add.existing(this.player)
     this.physics.add.existing(this.player)
@@ -93,24 +90,15 @@ class TestSheepMove extends Phaser.Scene {
     // set tree depth
     this.testTree.depth = this.testTree.y + this.testTree.height / 2
 
-    // Adds woolf enemy to scene and set up physics
-    this.add.existing(this.testWoolf)
-    this.physics.add.existing(this.testWoolf)
-    this.testWoolf.body.setSize(250, 180, true)
-    // this.testWoolf = this.physics.add.staticGroup()
-    this.testWoolf.body.setImmovable(true)
-    this.testWoolf.body.allowGravity = false
-
+    // set collision
     this.physics.add.collider(this.player, this.testTree)
-<<<<<<< HEAD
-=======
     this.physics.add.collider(this.player, this.testWoolf)
     // this.game.physics.arcade.collide(this.player, this.testTree, this.testWoolf)
->>>>>>> 5a9f160b4b403915e73781d4da8e52fa6ca5762e
 
     // Setup the key objects
     this.setupKeyboard()
 
+    // old version before scenes were merged
     // this.scene.run('SheepYawn', { player: this.player })
     // this.scene.moveAbove('SheepYawn', 'SheepMove')
 
@@ -134,37 +122,8 @@ class TestSheepMove extends Phaser.Scene {
     // Setup 'space' key for interaction
     this.yawnKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
     this.yawnKey.on('down', this.createYawnBlast, this)
-    this.yawnKey.on('up', this.destroyYwanBlash, this)
+    this.yawnKey.on('up', this.destroyYawnBlast, this)
     this.yawnKey.oldDown = false
-  }
-
-  // Creates sheep yawn circle, add physics and setup collider
-  createYawnBlast () {
-    // Destroys previous sheep yawn circles if they exist
-    if (this.yawnBlast) { this.yawnBlast.destroy() }
-    // console.log('Space key is being pressed')
-    this.yawnBlast = this.add.ellipse(this.player.x, this.player.y + 40, 100, 100, 0xff0000, 0.3)
-    this.yawnBlast.setStrokeStyle(2)
-    this._yawn_scale = 1.0
-
-    //   this.yawnBlast.body.setSize(this.player.x, this.player.y + 40, true)
-    //   // this.yawnBlast = this.physics.add.staticGroup()
-    //   this.yawnBlast.body.setImmovable(true)
-    //   this.yawnBlast.body.allowGravity = false
-
-    // Set up physics and collider
-    this.physics.add.existing(this.yawnBlast)
-    this.yawnBlast.setCircle(100)
-    this.physics.add.collider(this.yawnBlast)
-  }
-
-  // Destroys sheep yawn circle if space key is not being pressed and
-  // Yawn blast circle already exists
-  destroyYwanBlash () {
-    // console.log('Space key released')
-    if (this.yawnBlast) {
-      this.yawnBlast.destroy()
-    }
   }
 
   update (time, delta) {
@@ -191,57 +150,9 @@ class TestSheepMove extends Phaser.Scene {
     }
 
     this.player.body.velocity.set(velocity.x, velocity.y)
-<<<<<<< HEAD
     this.player.depth = this.player.y + this.player.height / 2
 
     this.depthCheck(this.testTree)
-  }
-
-  depthCheck (myTree) {
-    if (myTree.depth > this.player.depth)
-    {
-    // remember to make this different with other plants
-      this.player.depth = myTree.depth - 1
-      // Might be behind or to the side of the tree
-      if ((this.player.x > (myTree.x + myTree.height / 2)) || (this.player.x > (myTree.x + myTree.height / 2)))
-      {
-      // not behind tree
-      // top left, top right, bottom left, bottom right
-        console.log('not behind tree' + this.player.x)
-        this.testTree.setAlpha(1, 1, 1, 1)
-      }
-      else
-      { 
-        // behind tree
-        console.log('behind tree' + this.player.x)
-        // top left, top right, bottom left, bottom right
-        myTree.setAlpha(0, 0, 1, 1)
-      }
-    }
-    else 
-    {
-      this.player.depth = myTree.depth + 1
-      myTree.setAlpha(1, 1, 1, 1)
-      this.sheepFootPos = this.player.body.position.y + this.PlayerHeight
-      this.testTreeTopCollide = myTree.body.position.y - this.halfTreeBodyHeight
-      this.testTreeBottomCollide = myTree.body.position.y + this.TreeBodyHeight
-      console.log('sheepFootPos' + this.sheepFootPos)
-      console.log('bottom' + this.testTreeBottomCollide)
-      console.log('top' + this.testTreeTopCollide)
-      if ((this.sheepFootPos < this.testTreeBottomCollide) && (this.sheepFootPos > this.testTreeTopCollide) && this.movingVertical === false)
-      {
-        // allow the player to collide with the tree stump
-        myTree.body.enable = true
-        this.physics.add.collider(this.player, myTree)
-      }
-      else
-      {
-        myTree.body.enable = false
-      }      
-    }  
-  }
-=======
->>>>>>> 5a9f160b4b403915e73781d4da8e52fa6ca5762e
 
     // Moves sheep yawn circle with player when
     // Arrowkeys/wsad keys are pressed
@@ -259,12 +170,88 @@ class TestSheepMove extends Phaser.Scene {
     }
 
     // Increases thickness of stroke for the circle
-    // To indicate the max circumferance has been
-    // achieved
+    // To indicate the max circumferance has been achieved
     if (this.yawnBlast && this.yawnBlast.scale >= this._yawn_size_check) {
       console.log('point has been found')
       this.yawnBlast.setStrokeStyle(4.7)
     }
+  }
+
+  // Creates sheep yawn circle, add physics and setup collider
+  createYawnBlast () {
+  // Destroys previous sheep yawn circles if they exist
+    if (this.yawnBlast) { this.yawnBlast.destroy() }
+    // console.log('Space key is being pressed')
+    this.yawnBlast = this.add.ellipse(this.player.x, this.player.y + 40, 100, 100, 0xff0000, 0.3)
+    this.yawnBlast.setStrokeStyle(2)
+    this._yawn_scale = 1.0
+
+    //   this.yawnBlast.body.setSize(this.player.x, this.player.y + 40, true)
+    //   this.yawnBlast = this.physics.add.staticGroup()
+    //   this.yawnBlast.body.setImmovable(true)
+    //   this.yawnBlast.body.allowGravity = false
+
+    // Set up physics and collider
+    this.physics.add.existing(this.yawnBlast)
+    // this.yawnBlast.setCircle(100)
+    this.physics.add.collider(this.yawnBlast)
+  }
+
+  // Destroys sheep yawn circle if space key is not being pressed and
+  // Yawn blast circle already exists
+  destroyYawnBlast () {
+    // console.log('Space key released')
+    if (this.yawnBlast) {
+      this.yawnBlast.destroy()
+    }
+  }
+
+  depthCheck (myTree) {
+    if (myTree.depth > this.player.depth)
+    {
+    // remember to make this different with other plants
+      this.player.depth = myTree.depth - 1
+      // Might be behind or to the side of the tree
+      // console.log('1) is it behind?')
+      if ((this.player.x > (myTree.x + myTree.height / 2)) || (this.player.x > (myTree.x + myTree.height / 2)))
+      {
+      // not behind tree
+      // top left, top right, bottom left, bottom right
+        // console.log('2 not behind tree')
+        this.testTree.setAlpha(1, 1, 1, 1)
+      }
+      else
+      { 
+        // behind tree
+        // console.log('3 behind tree')
+        // top left, top right, bottom left, bottom right
+        myTree.setAlpha(0, 0, 1, 1)
+      }
+    }
+    else 
+    {
+      // console.log('4 below or collide with tree?')
+      this.player.depth = myTree.depth + 1
+      myTree.setAlpha(1, 1, 1, 1)
+      this.sheepFootPos = this.player.body.position.y + this.PlayerHeight
+      this.testTreeTopCollide = myTree.body.position.y - this.halfTreeBodyHeight
+      this.testTreeBottomCollide = myTree.body.position.y + this.TreeBodyHeight
+      // console.log('sheepFootPos' + this.sheepFootPos)
+      // console.log('bottom' + this.testTreeBottomCollide)
+      // console.log('top' + this.testTreeTopCollide)
+      if ((this.sheepFootPos < this.testTreeBottomCollide) && (this.sheepFootPos > this.testTreeTopCollide) && this.movingVertical === false)
+      {
+        // console.log('5 can collide with tree stump')
+        // allow the player to collide with the tree stump
+        myTree.body.enable = true
+        this.physics.add.collider(this.player, myTree)
+      }
+      else
+      {
+        // console.log('6 can walk through tree stump')
+        myTree.body.enable = false
+      }      
+    }  
   }
 
   render () { }
