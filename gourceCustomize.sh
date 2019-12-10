a31fbebf6fbf955e28beee86561641fadfd16dc8
Seth Berrier <berriers@uwstout.edu>
2019-12-10 14:55:43 -0600

    Update to gource scripts

--- gourceCustomize.sh
+++ gourceCustomize.sh
@@ -1,6 +1,8 @@
 # Remove actions after a certain date (currently December SGX)
 cat gourceLog.txt | awk -F\| '$1<=1576108800' > gourceLog.temp
+sed -i.bak '/rexrainbow/d' ./gourceLog.temp
 mv gourceLog.temp gourceLog.txt
+rm gourceLog.temp.bak
 
 # Setup Project Name
 projName="Woolhelmina's Bedtime Roundup - Source Code"
