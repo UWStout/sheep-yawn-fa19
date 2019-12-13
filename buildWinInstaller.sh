464382fde68dfec5d9805089cf08fbcf6704cd20
Seth Berrier <berriers@uwstout.edu>
2019-12-13 16:24:03 -0600

    Win installer updates

--- buildWinInstaller.sh
+++ buildWinInstaller.sh
@@ -3,6 +3,7 @@
 # Customize for specific game
 dirName=sheep-yawn
 pkgName=woolhemina
+webName=Woolhemina_s_Bedtime_Round-Up
 
 # Move to dir and rename it
 pushd "./packaged/${dirName}/"
@@ -10,10 +11,10 @@ mv "./win64/${dirName}.exe" "./win64/${pkgName}.exe"
 mv win64 "${pkgName}"
 
 # Delete previous archive
-test "../${pkgName}-win64.zip" & rm "../${pkgName}-win64.zip"
+rm -f "../${webName}-win64.zip"
 
 # Create archive
-zip -r9 "../${pkgName}-win64.zip" "./${pkgName}"
+zip -r9 "../${webName}-win64.zip" "./${pkgName}"
 
 # Name back and move back
 mv "${pkgName}" win64
