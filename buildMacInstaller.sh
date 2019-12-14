f4ae19862f2a3f94cd0a9a8fa2b59f3f27802739
Seth Berrier <berriers@uwstout.edu>
2019-12-13 22:54:25 -0600

    Update buildMacInstaller.sh

--- buildMacInstaller.sh
+++ buildMacInstaller.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-
-appName="DemoGame"                    # Desired name of app
-appPkgName="uwstout-gdd325-gamebase"  # Name of packaged mac app (w/o .app)
+dirName=sheep-yawn
+pkgName=woolhemina
+webName=Woolhemina_s_Bedtime_Round-Up
 
 # Since create-dmg does not override, be sure to delete previous DMG
-test -f "packaged/${appName}-macOS.dmg" && rm "packaged/${appName}-macOS.dmg"
+rm -f "packaged/${webName}-macOS.dmg"
 
 # Create the DMG
 create-dmg \
-  --volname "${appName}" \
+  --volname "${pkgName}" \
   --volicon "assets/images/icon.icns" \
-  --background "assets/images/background.png" \
+  --background "../background.png" \
   --window-pos 200 120 \
   --window-size 800 400 \
   --icon-size 100 \
-  --icon "${appPkgName}.app" 170 190 \
-  --hide-extension "${appPkgName}.app" \
+  --icon "${dirName}.app" 170 190 \
+  --hide-extension "${dirName}.app" \
   --app-drop-link 633 185 \
-  "packaged/${appName}-macOS.dmg" \
-  "packaged/${appPkgName}/osx64/${appPkgName}.app"
+  "packaged/${webName}-macOS.dmg" \
+  "packaged/${dirName}/osx64/${dirName}.app"
